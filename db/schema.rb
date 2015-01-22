b8df96c73b826747ac9ba01ed039f8573b90476d
lady3bean <annehawleybrett@gmail.com>
2015-01-22 17:21:51 -0500

    locations have color and therefore fail more test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122221704) do
+ActiveRecord::Schema.define(version: 20150122222021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150122221704) do
     t.text  "name"
     t.text  "city"
     t.text  "state"
+    t.text  "color"
   end
 
 end
