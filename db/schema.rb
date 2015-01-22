e837115667cf7be7cea0d0ac9beea71d5c6431da
lady3bean <annehawleybrett@gmail.com>
2015-01-22 17:19:10 -0500

    added city and state to locationas

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150122221704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.float "lat"
+    t.float "long"
+    t.text  "name"
+    t.text  "city"
+    t.text  "state"
+  end
 
 end
